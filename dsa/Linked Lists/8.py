d2c516451640f36da2df7c8bdf0dc596b83913a2
thewinterknight14@gmail.com <thewinterknight@gmail.com>
2019-09-11 17:57:47 +0530

    Complete Binary Tree implementation

--- dsa/Linked Lists/8.py
+++ dsa/Linked Lists/8.py
@@ -2,168 +2,168 @@
 
 class Node:
 
-	def __init__(self, data=None, next=None):
-		self.data = data
-		self.next = next
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
 
-	def get_data(self):
-		return self.data
+    def get_data(self):
+        return self.data
 
-	def get_next(self):
-		return self.next
+    def get_next(self):
+        return self.next
 
-	def set_next(self, new_node):
-		self.next = new_node
+    def set_next(self, new_node):
+        self.next = new_node
 
 
 class LinkedList(Node):
 
-	def __init__(self, head=None):
-		self.head = head
+    def __init__(self, head=None):
+        self.head = head
 
-	def insert_node(self, data):
-		new_node = Node(data)
+    def insert_node(self, data):
+        new_node = Node(data)
 
-		if self.head is None:
-			self.head = new_node
-		else:
-			ptr = self.head
-			while ptr.get_next() is not None:
-				ptr = ptr.get_next()
-			ptr.set_next(new_node)
+        if self.head is None:
+            self.head = new_node
+        else:
+            ptr = self.head
+            while ptr.get_next() is not None:
+                ptr = ptr.get_next()
+            ptr.set_next(new_node)
 
-	def print_linked_list(self):
-		if self.head is None:
-			print("Nothing to print\n")
-		else:
-			ptr = self.head
-			while ptr is not None:
-				print(ptr.get_data())
-				ptr = ptr.get_next()
+    def print_linked_list(self):
+        if self.head is None:
+            print("Nothing to print\n")
+        else:
+            ptr = self.head
+            while ptr is not None:
+                print(ptr.get_data())
+                ptr = ptr.get_next()
 
-	def reverse_print_linked_list(self):
-		self.reverse_print_util(self.head)
+    def reverse_print_linked_list(self):
+        self.reverse_print_util(self.head)
 
-	def reverse_print_util(self, ptr):
-		if ptr is None:
-			return
-		else:
-			self.reverse_print_util(ptr.get_next())
-			print(ptr.get_data())
+    def reverse_print_util(self, ptr):
+        if ptr is None:
+            return
+        else:
+            self.reverse_print_util(ptr.get_next())
+            print(ptr.get_data())
 
-	def get_length(self):
-		if self.head is None:
-			return 0
+    def get_length(self):
+        if self.head is None:
+            return 0
 
-		i = 0
-		ptr = self.head
-		while ptr is not None:
-			i += 1
-			ptr = ptr.get_next()
+        i = 0
+        ptr = self.head
+        while ptr is not None:
+            i += 1
+            ptr = ptr.get_next()
 
-		return i
+        return i
 
 
 def get_truncated_list(linked_list, truncate_node):
-	if linked_list.head is None:
-		return None
+    if linked_list.head is None:
+        return None
 
-	ptr = linked_list.head
-	while ptr.get_next() is not truncate_node:
-		ptr = ptr.get_next()
-	ptr.set_next(None)
+    ptr = linked_list.head
+    while ptr.get_next() is not truncate_node:
+        ptr = ptr.get_next()
+    ptr.set_next(None)
 
-	return LinkedList(linked_list.head)
+    return LinkedList(linked_list.head)
 
 
 def generate_equal_list(list_long, diff):
-	i = 0
-	equal_list_ptr = list_long.head
-	while i < diff:
-		equal_list_ptr = equal_list_ptr.get_next()
-		i += 1
+    i = 0
+    equal_list_ptr = list_long.head
+    while i < diff:
+        equal_list_ptr = equal_list_ptr.get_next()
+        i += 1
 
-	return LinkedList(equal_list_ptr), equal_list_ptr
+    return LinkedList(equal_list_ptr), equal_list_ptr
 
 
 def insert_leftover_carry(carry):
-	while carry / 10 != 0:
-		sum_linked_list.insert_node(carry % 10)
-		carry = int(carry / 10)
+    while carry / 10 != 0:
+        sum_linked_list.insert_node(carry % 10)
+        carry = int(carry / 10)
 
 
 def insert_leftover_operand(ptr, carry):
-	if ptr is None:
-		return 0, carry
+    if ptr is None:
+        return 0, carry
 
-	_, prev_carry = insert_leftover_operand(ptr.get_next(), carry)
+    _, prev_carry = insert_leftover_operand(ptr.get_next(), carry)
 
-	sum = ptr.get_data() + prev_carry
-	digit_sum = sum % 10
-	digit_carry = int(sum / 10)
+    sum = ptr.get_data() + prev_carry
+    digit_sum = sum % 10
+    digit_carry = int(sum / 10)
 
-	global sum_linked_list
-	sum_linked_list.insert_node(digit_sum)
+    global sum_linked_list
+    sum_linked_list.insert_node(digit_sum)
 
-	return digit_sum, digit_carry
+    return digit_sum, digit_carry
 
 
 def recursive_elementwise_sum(list1, list2):
-	len1 = list1.get_length()
-	len2 = list2.get_length()
+    len1 = list1.get_length()
+    len2 = list2.get_length()
 
-	if len1 > len2:
-		equal_list1, equal_list_ptr1 = generate_equal_list(list1, len1-len2)
-		_, carry = recursion_util(equal_list1.head, list2.head)
-		overflow_list = get_truncated_list(list1, equal_list_ptr1)
-		_, carry = insert_leftover_operand(overflow_list.head, carry)
-	elif len1 < len2:
-		equal_list2, equal_list_ptr2 = generate_equal_list(list2, len2-len1)
-		_, carry = recursion_util(list1.head, equal_list2.head)
-		overflow_list = get_truncated_list(list2, equal_list_ptr2)
-		_, carry = insert_leftover_operand(overflow_list.head, carry)
-	else:
-		_, carry = recursion_util(list1.head, list2.head)
+    if len1 > len2:
+        equal_list1, equal_list_ptr1 = generate_equal_list(list1, len1 - len2)
+        _, carry = recursion_util(equal_list1.head, list2.head)
+        overflow_list = get_truncated_list(list1, equal_list_ptr1)
+        _, carry = insert_leftover_operand(overflow_list.head, carry)
+    elif len1 < len2:
+        equal_list2, equal_list_ptr2 = generate_equal_list(list2, len2 - len1)
+        _, carry = recursion_util(list1.head, equal_list2.head)
+        overflow_list = get_truncated_list(list2, equal_list_ptr2)
+        _, carry = insert_leftover_operand(overflow_list.head, carry)
+    else:
+        _, carry = recursion_util(list1.head, list2.head)
 
-	insert_leftover_carry(carry)
+    insert_leftover_carry(carry)
 
 
 def recursion_util(ptr1, ptr2):
-	if ptr1 is None: 	# ...or if ptr2 is None
-		return 0, 0
+    if ptr1 is None:  # ...or if ptr2 is None
+        return 0, 0
 
-	_, prev_carry = recursion_util(ptr1.get_next(), ptr2.get_next())
+    _, prev_carry = recursion_util(ptr1.get_next(), ptr2.get_next())
 
-	sum = ptr1.get_data() + ptr2.get_data() + prev_carry
-	digit_sum = sum % 10
-	digit_carry = int(sum / 10)
+    sum = ptr1.get_data() + ptr2.get_data() + prev_carry
+    digit_sum = sum % 10
+    digit_carry = int(sum / 10)
 
-	global sum_linked_list
-	sum_linked_list.insert_node(digit_sum)
+    global sum_linked_list
+    sum_linked_list.insert_node(digit_sum)
 
-	return digit_sum, digit_carry
+    return digit_sum, digit_carry
 
 
 if __name__ == "__main__":
-	list1 = LinkedList()
-	list2 = LinkedList()
+    list1 = LinkedList()
+    list2 = LinkedList()
 
-	list1.insert_node(9)
-	list1.insert_node(9)
-	list1.insert_node(9)
+    list1.insert_node(9)
+    list1.insert_node(9)
+    list1.insert_node(9)
 
-	list2.insert_node(1)
-	list2.insert_node(8)
+    list2.insert_node(1)
+    list2.insert_node(8)
 
-	print('Linked list 1 -->\n')
-	list1.print_linked_list()
-	
-	print('\n\nLinked list 2 -->\n')
-	list2.print_linked_list()
+    print('Linked list 1 -->\n')
+    list1.print_linked_list()
 
-	sum_linked_list = LinkedList()
+    print('\n\nLinked list 2 -->\n')
+    list2.print_linked_list()
 
-	recursive_elementwise_sum(list1, list2)
+    sum_linked_list = LinkedList()
 
-	print('Sum linked list :\n')
-	sum_linked_list.reverse_print_linked_list()
+    recursive_elementwise_sum(list1, list2)
+
+    print('Sum linked list :\n')
+    sum_linked_list.reverse_print_linked_list()
---
+++ dsa/Trees/1.py
@@ -0,0 +1,144 @@
+# How is a complete binary search tree implemented?
+
+import random
+
+
+class QueueNode:
+
+	def __init__(self, tree_node=None, next=None):
+		self.tree_node = tree_node
+		self.next = next
+
+	def get_tree_node(self):
+		return self.tree_node
+
+	def get_next(self):
+		return self.next
+
+	def set_next(self, new_node):
+		self.next = new_node
+
+
+class Queue(QueueNode):
+
+	def __init__(self, head=None, tail=None):
+		# super().__init__()
+		self.head = head
+		self.tail = tail
+
+	def Enqueue(self, tree_node):
+		new_node = QueueNode(tree_node)
+
+		if self.tail is None and self.head is None:
+			self.head = new_node
+			self.tail = new_node
+		else:
+			self.tail.set_next(new_node)
+			self.tail = self.tail.get_next()
+
+	def get_front(self):
+		return self.head
+
+	def Dequeue(self):
+		if self.head is None:
+			return None
+
+		pop_node = self.head
+		self.head = self.head.get_next()
+		if self.head is None:
+			self.tail = None
+
+		return pop_node.get_tree_node()
+
+	def is_empty(self):
+		if self.head is None and self.tail is None:
+			return True
+		else:
+			return False
+
+
+class TreeNode:
+
+	def __init__(self, data=None, left=None, right=None):
+		self.data = data
+		self.left = left
+		self.right = right
+
+	def get_data(self):
+		return self.data
+
+	def get_left(self):
+		return self.left
+
+	def get_right(self):
+		return self.right
+
+	def set_left(self, left_node):
+		self.left = left_node
+
+	def set_right(self, right_node):
+		self.right = right_node
+
+
+class BinaryTree(TreeNode):
+
+	def __init__(self, root=None):
+		# super().__init__()
+		self.root = root
+
+	def level_order_traversal(self):
+		if self.root is None:
+			print("Tree does not exist\n")
+
+		traversal_queue = Queue()
+		traversal_queue.Enqueue(self.root)
+
+		while traversal_queue.is_empty() is False:
+			tree_node = traversal_queue.Dequeue()
+			print(tree_node.get_data())
+
+			if tree_node.get_left() is not None:
+				traversal_queue.Enqueue(tree_node.get_left())
+			if tree_node.get_right() is not None:
+				traversal_queue.Enqueue(tree_node.get_right())
+
+
+def insert_node(binary_tree, data, queue):
+	new_node = TreeNode(data)
+
+	if binary_tree.root is None:
+		binary_tree.root = new_node
+	else:
+		queue_front = queue.get_front()		# queue_front is a tree node that was put in the queue
+		tree_node = queue_front.get_tree_node()
+
+		if tree_node.get_left() is None:
+			tree_node.set_left(new_node)
+		elif tree_node.get_right() is None:
+			tree_node.set_right(new_node)
+
+		if tree_node.get_left() is not None and tree_node.get_right() is not None:
+			queue.Dequeue()
+
+	queue.Enqueue(new_node)
+
+
+if __name__ == "__main__":
+	binary_tree = BinaryTree()
+	queue = Queue()
+
+	arr = list(range(1, 12))
+	random.shuffle(arr)
+
+	print(arr)
+
+	print("Converting to a complete binary tree...\n\n")
+
+	for i in arr:
+		insert_node(binary_tree, i, queue)
+	binary_tree.level_order_traversal()
+
+
+
+
+
