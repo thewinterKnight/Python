0122bd00618114f80a99226784e9b00bbb5bcbf4
thewinterknight <thewinterknight14@gmail.com>
2019-10-01 16:48:47 +0530

    Implemented efficient lcm.

--- coursera/dsa - UCSD/Course 1/week2_algorithmic_warmup/4_least_common_multiple/lcm.py
+++ coursera/dsa - UCSD/Course 1/week2_algorithmic_warmup/4_least_common_multiple/lcm.py
@@ -1,6 +1,18 @@
 # Uses python3
 import sys
 
+
+def hcf(a, b):
+	if b == 0:
+		return a
+
+	return hcf(b, a % b)
+
+
+def lcm(a, b):
+	return (a * b) / hcf(a, b)
+
+
 def lcm_naive(a, b):
     for l in range(1, a*b + 1):
         if l % a == 0 and l % b == 0:
@@ -8,8 +20,12 @@ def lcm_naive(a, b):
 
     return a*b
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    a, b = map(int, input.split())
-    print(lcm_naive(a, b))
 
+if __name__ == '__main__':
+    # input = sys.stdin.read()
+    # a, b = map(int, input.split())
+    # print(lcm_naive(a, b))
+
+    a = int(input())
+    b = int(input())
+    print(lcm(a, b))
