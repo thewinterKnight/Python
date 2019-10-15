4a3cf831f207d5b99e6479b4f574fa62521f9909
thewinterknight <thewinterknight14@gmail.com>
2019-10-15 21:38:33 +0530

    Writing equilibrium sub-array code.

---
+++ dsa/miscellaneous/array_equilibrium_point.py
@@ -0,0 +1,30 @@
+
+
+def equilibrium_point(arr, req_sum):
+	sum_map = {}
+
+
+
+def main():
+	# print("Number of elements : ")
+	# n = int(input())
+
+	# arr = []
+	# print("Enter elements :")
+	# for i in range(0, n):
+	# 	arr.append(int(input()))
+
+	# print("Enter sum value : ")
+	# sum = int(input())
+	# print('\nInput array : {}'.format(arr))
+
+	arr = [2,6,-4,8,1,-6,9]
+	subarray_sum = -1
+
+	eq_arrays = equilibrium_point(arr, subarray_sum)
+	print()
+
+
+
+if __name__=="__main__":
+	main()
